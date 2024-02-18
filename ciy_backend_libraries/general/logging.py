812b6cc69d48d8d9ca8df86737108a97937e1c6e
ronen4822 <ronen4822@gmail.com>
2024-02-18 12:34:46 +0200

    -reduce library trash

--- ciy_backend_libraries/general/logging.py
+++ ciy_backend_libraries/general/logging.py
@@ -2,15 +2,13 @@ import logging
 from typing import Final
 from axiom_logger import AxiomHandler
 
-from ciy_backend_libraries.environment.env_supplier import EnvironmentSupplier
 
 LOGGING_FORMAT: Final[str] = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
 
 
 def initialize_logger(logger_name: str):
     formatter = logging.Formatter(LOGGING_FORMAT)
-    logger_details = EnvironmentSupplier().get_axiom_logger_details()
-    axiom_handler = AxiomHandler(logger_details.dataset, logger_details.api_token)
+    axiom_handler = AxiomHandler('backend-logs', 'xaat-3ec687a1-e15c-488a-acba-4780b9f8ca62')
     axiom_handler.setLevel(logging.INFO)
 
     logger = logging.getLogger(logger_name)
